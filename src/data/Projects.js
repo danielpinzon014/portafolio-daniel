164366c97af216504ddff3e3322738aea891a9a2
Daniel Pinzón <48761874+danielpinzon014@users.noreply.github.com>
2023-07-27 18:23:14 -0500

    Add files via upload

---
+++ src/data/Projects.js
@@ -0,0 +1,39 @@
+export const projects = [
+    {
+
+      id:'Delidioma',
+      title: 'Delidioma',
+      img: "https://mir-s3-cdn-cf.behance.net/projects/808/7cf2be166228511.Y3JvcCw4MDgsNjMyLDAsMA.png",
+      description: 'Figma designs for Delidioma',
+      type:"Design",
+      tec:"Figma",
+      url_project:"https://www.behance.net/gallery/166228511/Propuestas-web-Delidioma"
+    },
+    {
+      id:'soccer-planet',
+      title: 'Soccer-planet 360',
+      img: "https://mir-s3-cdn-cf.behance.net/projects/808/9eb5c6166011397.Y3JvcCw4MDgsNjMyLDAsMA.png",
+      description: 'Figma designs for Soccer planet 360',
+      type:"Design",
+      tec:"Figma",
+      url_project:"https://www.behance.net/gallery/166011397/Prototipos-web-Soccer-planet-360"
+    },
+    {
+      id:'Gel-tol',
+      title: 'GelTol',
+      img: "https://mir-s3-cdn-cf.behance.net/projects/808/34fb39156226905.Y3JvcCw4MDgsNjMyLDAsMA.jpg",
+      description: 'It`s a vending machine app, this project was created for academic purposes, the users select that they need in the app, and using his university card and cash, the vending machine throws them what they ordered through the app',
+      type:"Design",
+      tec:"Figma",
+      url_project:"https://www.behance.net/gallery/156226905/Geltol-machine-app"
+    },
+    {
+      id:'Herencia',
+      title: 'Herencia',
+      img: "https://mir-s3-cdn-cf.behance.net/projects/808/fab939159772095.Y3JvcCwxNDUyLDExMzYsMjgsMA.png",
+      description: 'Herencia is a tradicional candy store in Villa de Leyva, Boyacá. tThis project is a design proposal for a website',
+      type:"Design",
+      tec:"Figma",
+      url_project:"https://www.behance.net/gallery/159772095/Herencia-Neubrutalism"
+    },
+  ]
