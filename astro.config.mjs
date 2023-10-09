5c45833d8dfed5b1f17ab02ec32cb50d04310043
Daniel Pinzón <Tecplay34@gmail.com>
2023-10-09 08:40:52 -0500

    cambios

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,8 @@ import netlify from "@astrojs/netlify/functions";
 export default defineConfig({
   integrations: [tailwind()],
   output: 'server',
-  adapter: netlify()
+  adapter: netlify(),
+  experimental:{
+    viewTransitions:true
+  }
 });
